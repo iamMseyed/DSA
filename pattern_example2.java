ea3fc7c26ecd60e122db3983c6d1162ca9ff3f36
Seyed Muzafar <iamMseyed@users.noreply.github.com>
2024-12-22 13:36:45 +0530

    pattern_example2 - * equal to number of rows .java

---
+++ pattern_example2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("""
+        Printing pattern like : (whatever row, same *)
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+        ......
+        """);
+        System.out.print("Enter how many rows you want to get printed:");
+        int row = s.nextInt();
+        printPattern(row);
+    }
+    public static void printPattern(int rows){
+        for(int i = 0; i<rows;i++){
+            for(int j = 0; j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
